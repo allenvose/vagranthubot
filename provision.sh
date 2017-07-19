27f46305c77157271a75a4c85a1215d0b947080e
LAPTOP-R2LT65GG\avose <allenvose@gmial.com>
2017-07-19 13:52:34 -0400

    Updated provision to basic command exection and logging,  updates system, setup nodejs, git, fixes npm path for install acces, and installs NMP packages.

--- provision.sh
+++ provision.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+echo "Installing: nodejs, lynx, ruby and jekyll..."
 apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get -y update
-dpkg -s nodejs &>/dev/null || {
-	sudo apt-get -y install nodejs
-	ln -s /usr/bin/nodejs /usr/bin/node	
-}
-command -v yo &>/dev/null ||{
-	npm install -g yo coffee-script
-}
 
-command -v generator-hubot &>/dev/null ||{
-	npm install -g generator-hubot
-}
-mkdir /vagrant/myhubot
+apt-get update -y 
+apt-get install git - y >>/tmp/provision-script.log 2>&1
+apt-get install nodejs -y  >>/tmp/provision-script.log 2>&1
+
+mkdir /home/vagrant/.npm-global
+npm config set prefix '/home/vagrant/.npm-global'
+echo "export PATH=/home/vagrant/.npm-global/bin:$PATH" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+npm install -g yo coffee-script generator-hubot escodegen >>/tmp/provision-script.log 2>&1
+
+
+
+
+
+
